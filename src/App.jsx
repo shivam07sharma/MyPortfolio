f2107ff6010904fb6311b4c9c4b0c6ebc39985bf
shivam07sharma <shivam0701sharma@gmail.com>
2024-05-18 12:33:08 +0530

    img not showing 2

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,14 @@ function Homepage(){
             <div className='whitespace-nowrap overflow-hidden typing-animation text-cyan-300'  >Web Developer</div>
           </div>
           <div className=' flex mt-16 '>
-            <a href="src/components/SHIVAM SHARMA.pdf">  <div className="w-max h-9 px-2 bg-[#050a44] hover:bg-red-900 my-auto border-white border-2 rounded-lg flex items-center text-white text-base">Download Resume</div>
+            <a href="/components/SHIVAM SHARMA.pdf">  <div className="w-max h-9 px-2 bg-[#050a44] hover:bg-red-900 my-auto border-white border-2 rounded-lg flex items-center text-white text-base">Download Resume</div>
             </a>    <a href="https://www.github.com/shivam07sharma"> <div className="w-max h-9 px-2 bg-[#050a44]  hover:bg-red-900 mx-4 my-auto border-white border-2 rounded-lg flex items-center  text-white text-base">Visit Github</div></a>
 
           </div>
         </div>
       </div>
       <div className=' w-screen md:w-1/2 h-auto lg:h-[90vh] grid place-content-center'>
-        <img src="src/components/image1.jpg" alt="" className=' mx-auto my-auto mix-blend-lighten w-8/12 h-auto bg-cover rounded-full ' style={{boxShadow:"0px 0px 40px rgb(103 232 249)"}} />
+        <img src="/components/image1.jpg" alt="" className=' mx-auto my-auto mix-blend-lighten w-8/12 h-auto bg-cover rounded-full ' style={{boxShadow:"0px 0px 40px rgb(103 232 249)"}} />
       </div>
       </div>
   </>)
