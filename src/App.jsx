1c06d505d0024aa4549ad4aa3a30e45c7cb8d4e0
shivam07sharma <shivam0701sharma@gmail.com>
2024-05-22 14:31:23 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function Homepage(){
             <div className='whitespace-nowrap overflow-hidden typing-animation text-cyan-300'  >Web Developer</div>
           </div>
           <div className=' flex mt-16 '>
-          <a href="src\assets\SHIVAM SHARMA.pdf">  <div className="w-max h-9 px-2 bg-[#050a44] hover:bg-red-900 my-auto border-white border-2 rounded-lg flex items-center text-white text-base">Download Resume</div>
+          <a href="src//assets//SHIVAM SHARMA.pdf">  <div className="w-max h-9 px-2 bg-[#050a44] hover:bg-red-900 my-auto border-white border-2 rounded-lg flex items-center text-white text-base">Download Resume</div>
             </a>    <a href="https://www.github.com/shivam07sharma"> <div className="w-max h-9 px-2 bg-[#050a44]  hover:bg-red-900 mx-4 my-auto border-white border-2 rounded-lg flex items-center  text-white text-base">Visit Github</div></a>
 
           </div>
@@ -26,7 +26,7 @@ function Homepage(){
       </div>
       <div className=' w-screen md:w-1/2 h-auto lg:h-[90vh] grid place-content-center'>
        
-     <img src="src//components//image1.jpg" alt="image 1" className=' mx-auto my-auto mix-blend-lighten w-8/12 h-auto bg-cover rounded-full get-transition' style={{ boxShadow:"0px 0px 40px rgb(103 232 249)"}} />
+       <img src="https://media.istockphoto.com/id/1178763127/vector/man-with-laptop-sitting-on-the-chair-freelance-or-studying-concept-cute-illustration-in-flat.jpg?s=612x612&w=0&k=20&c=gzk5c0q1DkndI2IFHIBCHIapEiFHm6JuG0-6C3xL-3I=" alt="" className=' mx-auto my-auto mix-blend-lighten w-8/12 h-auto bg-cover rounded-full get-transition' style={{ boxShadow:"0px 0px 40px rgb(103 232 249)"}} />
     
       </div>
       </div>
